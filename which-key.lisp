0299d0c542b81e68bce8e25174194ece8e9c79c5
Dany Haddad <danyhaddad43@gmail.com>
2016-12-11 18:48:28 -0800

    make usage simpler

--- which-key.lisp
+++ which-key.lisp
@@ -1,21 +1,17 @@
 (defun get-keymaps-at-key (keymaps key)
-  (reduce
-   (lambda (result map)
-     (let* ((binding (find key (kmap-bindings map)
-                          :key 'binding-key :test 'equalp))
-           (command (when binding (binding-command binding))))
-       (if command
-           (setf result (cons command result))
-           result)))
-   keymaps
-   :initial-value '()))
+  (dereference-kmaps
+   (reduce
+    (lambda (result map)
+      (let* ((binding (find key (kmap-bindings map)
+                            :key 'binding-key :test 'equalp))
+             (command (when binding (binding-command binding))))
+        (if command
+            (setf result (cons command result))
+            result)))
+    keymaps
+    :initial-value '())))
 
 (defun get-keymaps-at-key-seq (keymaps key-seq)
   (if (= 1 (length key-seq))
-      (get-keymaps-at-key (dereference-kmaps keymaps) (first key-seq))
-      (get-keymaps-at-key-seq (get-keymaps-at-key (dereference-kmaps keymaps) (first key-seq)) (rest key-seq))))
-
-;; (get-keymaps-at-key-seq (top-maps) `(,(kbd "C-t") ,(kbd "g")))
-
-;; (apply 'display-bindings-for-keymaps (reverse `(,(kbd "C-t") ,(kbd "g")))
-;;        (dereference-kmaps (get-keymaps-at-key-seq (dereference-kmaps (top-maps)) `(,(kbd "C-t") ,(kbd "g")))))
+      (get-keymaps-at-key keymaps (first key-seq))
+      (get-keymaps-at-key-seq (get-keymaps-at-key keymaps (first key-seq)) (rest key-seq))))
