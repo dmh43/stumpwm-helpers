39c50698ab41778566932a920d5f55ca36a6293b
Dany Haddad <danyhaddad43@gmail.com>
2016-12-14 21:27:55 -0800

    simple killer

--- navigation-helpers.lisp
+++ navigation-helpers.lisp
@@ -4,16 +4,24 @@
   (set-x-selection "")
   (labels ((repeater (ctr)
              (sleep 0.001)
-             (stumpwm::send-meta-key (current-screen) (kbd "S-C-b"))
+             (stumpwm::send-meta-key (current-screen) (kbd "S-Left"))
              (let* ((sel (get-x-selection))
                     (start-pos (search " " sel)))
                (if start-pos
                    (progn
                      (loop
                         for i from 0 to ctr
-                        do (stumpwm::send-meta-key (current-screen) (kbd "C-f")))
+                        do (stumpwm::send-meta-key (current-screen) (kbd "Right")))
                      (subseq sel (+ start-pos 1)))
                    (repeater (+ 1 ctr))))))
     (repeater 0)))
 
-(message (chars-before-space-left))
+(defun kill-till-space-left ()
+  (let* ((chars-left (chars-before-space-left))
+         (num-chars (length chars-left)))
+    (sleep 0.5)
+    (loop
+       for i from 0 to num-chars
+       do (progn
+            (sleep 0.1)
+            (stumpwm::send-meta-key (current-screen) (kbd "DEL"))))))
