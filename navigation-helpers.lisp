9ef53b4d7de9a9b5aeadbb3f611196434d207ba7
Dany Haddad <danyhaddad43@gmail.com>
2016-12-14 21:02:19 -0800

    add basic chars-before-space-left

--- navigation-helpers.lisp
+++ navigation-helpers.lisp
@@ -1,13 +1,19 @@
 (defun chars-before-space-right ())
 
 (defun chars-before-space-left ()
-  (labels ((repeater ()
+  (set-x-selection "")
+  (labels ((repeater (ctr)
+             (sleep 0.001)
              (stumpwm::send-meta-key (current-screen) (kbd "S-C-b"))
              (let* ((sel (get-x-selection))
                     (start-pos (search " " sel)))
                (if start-pos
-                   (subseq sel (+ start-pos 1))
-                   (repeater)))))
-    (repeater)))
+                   (progn
+                     (loop
+                        for i from 0 to ctr
+                        do (stumpwm::send-meta-key (current-screen) (kbd "C-f")))
+                     (subseq sel (+ start-pos 1)))
+                   (repeater (+ 1 ctr))))))
+    (repeater 0)))
 
 (message (chars-before-space-left))
