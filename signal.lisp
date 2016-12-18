302171904e59a5b86e354ddc7a7dfe4d45f0f7c4
Dany Haddad <danyhaddad43@gmail.com>
2016-12-18 01:09:05 -0800

    signal handlers

---
+++ signal.lisp
@@ -0,0 +1,20 @@
+(ql:quickload :cffi)
+
+(defvar *SIGINT* 2)
+
+(defmacro set-signal-handler (signo &body body)
+  (let ((handler (gensym "HANDLER")))
+    `(progn
+       (cffi:defcallback ,handler :void ((signo :int))
+         (declare (ignore signo))
+         ,@body)
+       (cffi:foreign-funcall "signal" :int ,signo :pointer (cffi:callback ,handler)))))
+
+
+(set-signal-handler *SIGINT*
+  (with-open-file (str "~/stump.log"
+                       :direction :output
+                       :if-exists :supersede
+                       :if-does-not-exist :create)
+    (format str "~A" (prin1-to-string *last-unhandled-error*)))
+  (quit))
