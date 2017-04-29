56fa0549686446dd20e2ea7877b604ff0239c290
Dany Haddad <danyhaddad43@gmail.com>
2017-04-29 14:59:09 -0700

    use async call to write to cache

--- launcher.lisp
+++ launcher.lisp
@@ -1,7 +1,6 @@
 ;; lsdesktopf is available here: https://github.com/AndyCrowd/list-desktop-files
-(defun get-program-table ()
-  (let* ((response (run-shell-command "lsdesktopf" t))
-         (program-list (cl-ppcre:split "\\n" response))
+(defun parse-program-table (response)
+  (let* ((program-list (cl-ppcre:split "\\n" response))
          (valid-programs (remove-if (lambda (entry) (cl-ppcre:scan "^#" entry)) program-list))
          (table (mapcar (lambda (entry)
                           (let ((pair (cl-ppcre:split " *# *" entry)))
@@ -9,8 +8,28 @@
                         valid-programs)))
     (remove-duplicates table :test (lambda (pair1 pair2) (string= (cadr pair1) (cadr pair2))))))
 
+(defun cache-program-table (cache-path)
+  (with-open-file (s cache-path :direction :output :if-exists :supersede)
+    (write-string (run-shell-command "lsdesktopf" t) s)))
+
+(defun get-program-table (cache-path)
+  (with-open-file (s cache-path :if-does-not-exist nil)
+    (if (streamp s)
+        (let ((program-table (make-string (file-length s))))
+          (read-sequence program-table s)
+          (parse-program-table program-table))
+        (progn
+          (cache-program-table cache-path)
+          (get-program-table cache-path)))))
+
 (defun select-program-from-menu (program-table)
   (select-from-menu (current-screen) program-table "Run a program:"))
 
+(defun drop-file-specifier (desktop-entry)
+  (cl-ppcre:scan-to-strings "[^ ]+" desktop-entry))
+
 (defcommand launcher () (:rest)
-  (run-shell-command (cadr (select-program-from-menu (get-program-table)))))
+  (run-shell-command (drop-file-specifier
+                      (cadr (select-program-from-menu (get-program-table #P"~/deskcache")))))
+  (sb-thread:make-thread (lambda () (cache-program-table #P"~/deskcache")))
+  nil)
