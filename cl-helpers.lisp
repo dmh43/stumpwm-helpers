aeccc49592e12ed92db07f4fdcc3f42e229c17aa
Dany Haddad <danyhaddad43@gmail.com>
2017-04-29 01:45:06 -0700

    add launcher and cl-helpers

---
+++ cl-helpers.lisp
@@ -0,0 +1,26 @@
+(defun shallow-copy-object (original)
+  (let* ((class (class-of original))
+         (copy (allocate-instance class)))
+    (dolist (slot (mapcar #'slot-definition-name (class-slots class)))
+      (when (slot-boundp original slot)
+        (setf (slot-value copy slot)
+              (slot-value original slot))))
+    copy))
+
+(defgeneric copy-instance (object &rest initargs &key &allow-other-keys)
+  (:documentation "Makes and returns a shallow copy of OBJECT.
+
+  An uninitialized object of the same class as OBJECT is allocated by
+  calling ALLOCATE-INSTANCE.  For all slots returned by
+  CLASS-SLOTS, the returned object has the
+  same slot values and slot-unbound status as OBJECT.
+
+  REINITIALIZE-INSTANCE is called to update the copy with INITARGS.")
+  (:method ((object standard-object) &rest initargs &key &allow-other-keys)
+    (let* ((class (class-of object))
+           (copy (allocate-instance class)))
+      (dolist (slot-name (mapcar #'sb-mop:slot-definition-name (sb-mop:class-slots class)))
+        (when (slot-boundp object slot-name)
+          (setf (slot-value copy slot-name)
+                (slot-value object slot-name))))
+      (apply #'reinitialize-instance copy initargs))))
---
+++ launcher.lisp
@@ -0,0 +1,16 @@
+(defun get-program-table (path)
+  (let* ((response (with-output-to-string (s)
+                     (sb-ext:run-program "lsdesktopf" nil :search path :output s)))
+         (program-list (cl-ppcre:split "\\n" response))
+         (valid-programs (remove-if (lambda (entry) (cl-ppcre:scan "^#" entry)) program-list))
+         (table (mapcar (lambda (entry)
+                          (let ((pair (cl-ppcre:split " *# *" entry)))
+                            `(,(concatenate 'string (nth 1 pair) " " (car pair)) ,(car pair))))
+                        valid-programs)))
+    (remove-duplicates table :test (lambda (pair1 pair2) (string= (cadr pair1) (cadr pair2))))))
+
+(defun select-program-from-menu (program-table)
+  (select-from-menu (current-screen) program-table "Run a program:"))
+
+(defcommand launcher () (:rest)
+  (run-shell-command (cadr (select-program-from-menu (get-program-table #P"~/bin/")))))
